9ac680d52303c31535274654c48938f786255724
Benjamin Fransson <benjamin@cafcon.se>
2019-03-10 22:23:51 +0100

    Removed unnecessary comment

--- Game.cs
+++ Game.cs
@@ -5,16 +5,13 @@ namespace Pong {
         private int duration = 0;
         public Game () {
 
-            // Initialize game
             Console.Clear ();
             Console.CursorVisible = false;
+
             GameBoard board = new GameBoard (60, 16, 4, 2);
-
             Ball ball = new Ball(board.GetCenterHorizontal(), board.GetCenterVertical(), ConsoleColor.Green);
-
             Player player1 = new Player("John", board.GetBorderLeft() + 2, (board.GetCenterVertical()), ConsoleColor.Blue);
             Player player2 = new Player("John", board.GetBorderRight() - 2, (board.GetCenterVertical()), ConsoleColor.Red);
-            
             
             board.Draw ();
             ball.Draw();
