dfd4720bbb00ee5f138ce5b42c4f66091176bfe1
Benjamin Fransson <benjamin@cafcon.se>
2019-03-10 16:50:35 +0100

    Added Player Class

---
+++ Player.cs
@@ -0,0 +1,16 @@
+namespace Pong
+{
+    public class Player : Entity
+    {
+        private readonly string name;
+        private int score = 0;
+
+        public Player()
+        {
+        }
+
+        public void Draw(){
+
+        }
+    }
+}
