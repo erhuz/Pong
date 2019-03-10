c6518eab110ac2c7fc3b4caff63eac460e45441d
Benjamin Fransson <benjamin@cafcon.se>
2019-03-10 14:33:37 +0100

    Formatted code

--- GameBoard.cs
+++ GameBoard.cs
@@ -1,9 +1,7 @@
 using System;
 
-namespace Pong
-{
-    public class GameBoard
-    {
+namespace Pong {
+    public class GameBoard {
         private readonly int width;
         private readonly int height;
         private readonly int xOffset;
@@ -12,14 +10,14 @@ namespace Pong
         private ConsoleColor backgroundColor;
         private ConsoleColor outlineColor;
 
-        public GameBoard(
-                int width,
-                int height,
-                int xOffset = 2,
-                int yOffset = 2,
-                ConsoleColor backgroundColor = ConsoleColor.Black,
-                ConsoleColor outlineColor = ConsoleColor.White
-                ){
+        public GameBoard (
+            int width,
+            int height,
+            int xOffset = 2,
+            int yOffset = 2,
+            ConsoleColor backgroundColor = ConsoleColor.Black,
+            ConsoleColor outlineColor = ConsoleColor.White
+        ) {
 
             this.width = width;
             this.height = height;
@@ -29,43 +27,39 @@ namespace Pong
             this.outlineColor = outlineColor;
         }
 
-        public void Draw(){
+        public void Draw () {
 
-            Console.Clear();
-            
-            for (int y = 0; y < height + (this.yOffset * 2); y++)
-            {
-                for (int x = 0; x < (width + this.xOffset * 2); x++)
-                {
-                    Console.SetCursorPosition(x, y);
+            Console.Clear ();
+
+            for (int y = 0; y < height + (this.yOffset * 2); y++) {
+                for (int x = 0; x < (width + this.xOffset * 2); x++) {
+                    Console.SetCursorPosition (x, y);
                     Console.BackgroundColor = backgroundColor;
-                    Console.Write(" ");
+                    Console.Write (" ");
                 }
             }
 
-            for (int y = this.yOffset; y < height + this.yOffset; y++)
-            {
+            for (int y = this.yOffset; y < height + this.yOffset; y++) {
                 Console.BackgroundColor = outlineColor;
-                Console.SetCursorPosition(this.xOffset, y);
-                Console.Write(" ");
-                
-                Console.SetCursorPosition(this.xOffset + width - 1, y);
-                Console.Write(" ");
+                Console.SetCursorPosition (this.xOffset, y);
+                Console.Write (" ");
+
+                Console.SetCursorPosition (this.xOffset + width - 1, y);
+                Console.Write (" ");
             }
 
-            for (int x = this.xOffset; x < width + this.xOffset; x++)
-            {
+            for (int x = this.xOffset; x < width + this.xOffset; x++) {
                 Console.BackgroundColor = outlineColor;
-                Console.SetCursorPosition(x, this.yOffset);
-                Console.Write(" ");
-                
-                Console.SetCursorPosition(x, this.yOffset + height - 1);
-                Console.Write(" ");
+                Console.SetCursorPosition (x, this.yOffset);
+                Console.Write (" ");
+
+                Console.SetCursorPosition (x, this.yOffset + height - 1);
+                Console.Write (" ");
             }
 
             Console.BackgroundColor = backgroundColor;
-            Console.SetCursorPosition(0,0);
+            Console.SetCursorPosition (0, 0);
         }
-        
+
     }
 }
