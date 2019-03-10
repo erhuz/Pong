4d4e4575937cfeec154ff5e0901e016386b58dd4
Benjamin Fransson <benjamin@cafcon.se>
2019-03-10 22:19:49 +0100

    Added  Draw method to Ball Class

--- Ball.cs
+++ Ball.cs
@@ -4,14 +4,24 @@ namespace Pong
 {
     public class Ball : Entity
     {
-        public Ball(int xPos, int yPos)
+        public Ball(int xPos, int yPos, ConsoleColor color = ConsoleColor.White)
         {
             this.xPos = xPos;
             this.yPos = yPos;
+            this.color = color;
         }
 
         public void Draw(){
+            ConsoleColor initialColor = Console.BackgroundColor;
+            Console.BackgroundColor = color;
             
+            Console.SetCursorPosition(this.xPos - 1, this.yPos);
+            Console.Write(" ");
+            Console.SetCursorPosition(this.xPos, this.yPos);
+            Console.Write(" ");
+
+            Console.BackgroundColor = initialColor;
+            Console.SetCursorPosition (0, 0);
         }
     }
 }
