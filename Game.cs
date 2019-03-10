1ef01d3287776441b8c6ad4c30cf65f74174f407
Benjamin Fransson <benjamin@cafcon.se>
2019-03-10 22:20:10 +0100

    Refactored game initialization

--- Game.cs
+++ Game.cs
@@ -9,10 +9,15 @@ namespace Pong {
             Console.Clear ();
             Console.CursorVisible = false;
             GameBoard board = new GameBoard (60, 16, 4, 2);
-            board.Draw ();
 
-            Player player1 = new Player("John", board.GetBorderLeft() + 2, (board.GetBorderBottom()), ConsoleColor.Blue);
-            Player player2 = new Player("John", board.GetBorderRight() - 2, (board.GetBorderBottom()), ConsoleColor.Red);
+            Ball ball = new Ball(board.GetCenterHorizontal(), board.GetCenterVertical(), ConsoleColor.Green);
+
+            Player player1 = new Player("John", board.GetBorderLeft() + 2, (board.GetCenterVertical()), ConsoleColor.Blue);
+            Player player2 = new Player("John", board.GetBorderRight() - 2, (board.GetCenterVertical()), ConsoleColor.Red);
+            
+            
+            board.Draw ();
+            ball.Draw();
             player1.Draw();
             player2.Draw();
         }
