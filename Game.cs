e17cfa7d45f659699098f7f62fb6ae49941c850b
Benjamin Fransson <benjamin@cafcon.se>
2019-03-10 18:21:38 +0100

    test player draw

--- Game.cs
+++ Game.cs
@@ -10,6 +10,11 @@ namespace Pong {
             Console.CursorVisible = false;
             GameBoard board = new GameBoard (60, 16, 4, 2);
             board.Draw ();
+
+            Player player1 = new Player("John", board.GetBorderLeft() + 2, (board.GetBorderBottom()), ConsoleColor.Blue);
+            Player player2 = new Player("John", board.GetBorderRight() - 2, (board.GetBorderBottom()), ConsoleColor.Red);
+            player1.Draw();
+            player2.Draw();
         }
     }
 }
