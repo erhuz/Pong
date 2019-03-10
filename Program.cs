d03bacf0e07275cd118487bad81ca85e939d9fd7
Benjamin Fransson <benjamin@cafcon.se>
2019-03-10 14:33:57 +0100

    Added GameBoard Class

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 
-namespace Pong
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Pong {
+    class Program {
+        static void Main (string[] args) {
+
+
+
+            // Initialize game
+            Console.CursorVisible = false;
+            
+            GameBoard board = new GameBoard(60, 16, 4, 2);
+            board.Draw();
         }
     }
 }
