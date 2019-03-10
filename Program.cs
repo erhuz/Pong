a96e4139a899e72f0ebb40965b5302eebdf4d155
Benjamin Fransson <benjamin@cafcon.se>
2019-03-10 14:35:15 +0100

    Formatted code

--- Program.cs
+++ Program.cs
@@ -1,16 +1,13 @@
-﻿using System;
+using System;
 
 namespace Pong {
     class Program {
         static void Main (string[] args) {
-
-
-
             // Initialize game
             Console.CursorVisible = false;
-            
-            GameBoard board = new GameBoard(60, 16, 4, 2);
-            board.Draw();
+
+            GameBoard board = new GameBoard (60, 16, 4, 2);
+            board.Draw ();
         }
     }
 }
