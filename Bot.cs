d66652999c9f4348eb0c52c908ac07f3d5e30f0b
Benjamin Fransson <benjamin@cafcon.se>
2019-03-14 00:20:14 +0100

    Added some bot movement

--- Bot.cs
+++ Bot.cs
@@ -8,9 +8,17 @@ namespace Pong
     {
         public Bot(string name, int xPos, int yPos)
         : base(name, xPos, yPos, 3, ConsoleColor.Red){
-
+            this.speed = 1;
         }
 
         // Make some sort of movement method / algorithm
+        
+        public void ChangeDirection(){
+            this.speed *= -1;
+        }
+
+        public void Move(){
+            this.yPos += this.speed;
+        }
     }
 }
