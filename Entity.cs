8aafd145f35cc5478979d03f468ea4f286edec26
Benjamin Fransson <benjamin@cafcon.se>
2019-03-13 22:32:34 +0100

    Added methods: detect if entity did/will hit wall

--- Entity.cs
+++ Entity.cs
@@ -7,5 +7,33 @@ namespace Pong
         protected int xPos;
         protected int yPos;
         protected ConsoleColor color;
+
+
+        protected bool DidEntityHitBarrier(int top, int bottom, int left, int right){
+            if(
+                this.yPos <= top ||
+                this.yPos >= bottom ||
+                this.xPos <= left ||
+                this.xPos >= right
+            ){
+                return true;
+            }
+
+            return false;
+        }
+
+        protected bool WillEntityHitBarrier(int top, int bottom, int left, int right){
+            
+            if(
+                this.yPos - 1 <= top ||
+                this.yPos + 1 >= bottom ||
+                this.xPos + 1 <= left ||
+                this.xPos - 1 >= right
+            ){
+                return true;
+            }
+            
+            return false;
+        }
     }
 }
