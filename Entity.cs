0d2ec38610969e3bc91ace4ca55c0a4de610f344
Benjamin Fransson <benjamin@cafcon.se>
2019-03-14 15:12:20 +0100

    Removed unnecessary function

--- Entity.cs
+++ Entity.cs
@@ -1,38 +1,24 @@
 using System;
 
-namespace Pong
-{
-    public abstract class Entity
-    {
+namespace Pong {
+    public abstract class Entity {
         protected int xPos;
         protected int yPos;
         protected int speed;
         protected int size;
         protected ConsoleColor color;
 
+        public bool WillEntityHitBarrier (int top, int bottom, int left, int right) {
 
-        public bool DidEntityHitBarrier(int top, int bottom, int left, int right){
-            if(
-                this.yPos <= top ||
-                this.yPos >= bottom
-            ){
-                return true;
-            }
-
-            return false;
-        }
-
-        public bool WillEntityHitBarrier(int top, int bottom, int left, int right){
-            
-            if(
+            if (
                 this.yPos - 1 <= top ||
                 this.yPos + this.size >= bottom ||
                 this.xPos + 1 <= left ||
                 this.xPos - 1 >= right
-            ){
+            ) {
                 return true;
             }
-            
+
             return false;
         }
     }
