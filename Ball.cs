1d1c2bd08997e944b267f25c6961a4b17718cfa6
Benjamin Fransson <benjamin@cafcon.se>
2019-03-14 15:12:46 +0100

    Added x & y speed variables for natural motion

--- Ball.cs
+++ Ball.cs
@@ -1,24 +1,25 @@
 using System;
 
-namespace Pong
-{
-    public class Ball : Entity
-    {
-        public Ball(int xPos, int yPos, ConsoleColor color = ConsoleColor.White)
-        {
+namespace Pong {
+    public class Ball : Entity {
+
+        private int ySpeed;
+        private int xSpeed;
+
+        public Ball (int xPos, int yPos, ConsoleColor color = ConsoleColor.White) {
             this.xPos = xPos;
             this.yPos = yPos;
             this.color = color;
         }
 
-        public void Draw(){
+        public void Draw () {
             ConsoleColor initialColor = Console.BackgroundColor;
             Console.BackgroundColor = color;
-            
-            Console.SetCursorPosition(this.xPos - 1, this.yPos);
-            Console.Write(" ");
-            Console.SetCursorPosition(this.xPos, this.yPos);
-            Console.Write(" ");
+
+            Console.SetCursorPosition (this.xPos - 1, this.yPos);
+            Console.Write (" ");
+            Console.SetCursorPosition (this.xPos, this.yPos);
+            Console.Write (" ");
 
             Console.BackgroundColor = initialColor;
             Console.SetCursorPosition (0, 0);
