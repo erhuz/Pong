e44dec47b644218f23794fdd5b55fa19258aeb06
Benjamin Fransson <benjamin@cafcon.se>
2019-03-14 19:40:01 +0100

    Added abstract Draw() & CleanDraw() methods Moved collision checking to inheriting Classes

--- Entity.cs
+++ Entity.cs
@@ -8,32 +8,9 @@ namespace Pong {
         protected int size;
         protected ConsoleColor color;
 
-        public bool WillEntityHitBarrier (int top, int bottom, int left, int right) {
+        public abstract void Draw();
+        public abstract void CleanDraw();
 
-            if (
-                this.yPos - 1 <= top ||
-                this.yPos + this.size >= bottom ||
-                this.xPos + 1 <= left ||
-                this.xPos - 1 >= right
-            ) {
-                return true;
-            }
-
-            return false;
-        }
-
-        public string WhichBarrierWillEntityHit (int top, int bottom, int left, int right) {
-            if (this.yPos - 1 <= top) {
-                return "top";
-            } else if (this.yPos + this.size >= bottom) {
-                return "bottom";
-            } else if (this.xPos + 1 <= left) {
-                return "left";
-            } else if (this.xPos - 1 >= right) {
-                return "right";
-            } else {
-                return null;
-            }
-        }
+        
     }
 }
