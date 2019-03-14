be7e66aa8ea86018ed04527ce978ffa20b276cfb
Benjamin Fransson <benjamin@cafcon.se>
2019-03-14 15:15:42 +0100

    restructured Player constructor params

--- Bot.cs
+++ Bot.cs
@@ -2,22 +2,20 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Pong
-{
-    public class Bot : Player
-    {
-        public Bot(string name, int xPos, int yPos)
-        : base(name, xPos, yPos, 3, ConsoleColor.Red){
+namespace Pong {
+    public class Bot : Player {
+        public Bot (string name, int xPos, int yPos, int topBarrier, int bottomBarrier, int size = 3)
+         : base (name, xPos, yPos, topBarrier, bottomBarrier, size, ConsoleColor.Red) {
             this.speed = 1;
         }
 
         // Make some sort of movement method / algorithm
-        
-        public void ChangeDirection(){
+
+        public void ChangeDirection () {
             this.speed *= -1;
         }
 
-        public void Move(){
+        public void Move () {
             this.yPos += this.speed;
         }
     }
--- Player.cs
+++ Player.cs
@@ -1,50 +1,53 @@
 using System;
 
-namespace Pong
-{
-    public class Player : Entity
-    {
+namespace Pong {
+    public class Player : Entity {
         private readonly string name;
         private int score = 0;
+        protected int topBarrier;
+        protected int bottomBarrier;
 
-        public Player(
+        public Player (
             string name,
             int xPos,
             int yPos,
+            int topBarrier,
+            int bottomBarrier,
             int size = 3,
             ConsoleColor color = ConsoleColor.Blue
-        ){
-        
+        ) {
+
             this.name = name;
             this.xPos = xPos;
+            this.yPos = yPos - (size / 2); // Center player position vertically
+            this.topBarrier = topBarrier;
+            this.bottomBarrier = bottomBarrier;
             this.size = size;
             this.color = color;
-            this.yPos = yPos - (size / 2); // Center player position vertically
         }
 
         // public bool Move(int y){
         //     if(this.WillEntityHitBarrier())
         // }
 
-        public void Draw(){
+        public void Draw () {
             ConsoleColor initialColor = Console.BackgroundColor;
             Console.BackgroundColor = color;
 
-            for (int i = 0; i < this.size; i++)
-            {
-                Console.SetCursorPosition(this.xPos, this.yPos + i);
-                Console.Write(" ");
+            for (int i = 0; i < this.size; i++) {
+                Console.SetCursorPosition (this.xPos, this.yPos + i);
+                Console.Write (" ");
             }
 
             Console.BackgroundColor = initialColor;
             Console.SetCursorPosition (0, 0);
         }
 
-        public void CleanDraw(){
-                Console.SetCursorPosition(this.xPos, this.yPos);
-                Console.Write(" ");
-                Console.SetCursorPosition(this.xPos, this.yPos + this.size - 1);
-                Console.Write(" ");
+        public void CleanDraw () {
+            Console.SetCursorPosition (this.xPos, this.yPos);
+            Console.Write (" ");
+            Console.SetCursorPosition (this.xPos, this.yPos + this.size - 1);
+            Console.Write (" ");
         }
     }
 }
