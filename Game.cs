710c5d052103a29aade59f32654974922f60e938
Benjamin Fransson <benjamin@cafcon.se>
2019-03-14 15:16:21 +0100

    Added vars to control board size on a higher level

--- Game.cs
+++ Game.cs
@@ -5,60 +5,89 @@ using System.Threading.Tasks;
 namespace Pong {
     public class Game {
 
-        private int speed = 10;
+        private int speed = 100;
         private int duration;
+        private int playerSize = 4;
 
-        public Game (int width, int height, int xOffset, int yOffset) {
+        private int boardWidth = 60;
+        private int boardHeight = 16;
+        private int boardXOffset = 4;
+        private int boardYOffset = 2;
+
+        public Game () {
 
             Console.Clear ();
             Console.CursorVisible = false;
 
-            bool playing = true;
-
-            GameBoard board = new GameBoard (width, height, xOffset, yOffset);
-            Ball ball = new Ball(board.GetCenterHorizontal(), board.GetCenterVertical(), ConsoleColor.Green);
-            Player player1 = new Player("John", board.GetBorderLeft() + 2, (board.GetCenterVertical()), 3, ConsoleColor.Blue);
-            Bot player2 = new Bot("Bot", board.GetBorderRight() - 2, (board.GetCenterVertical()));
-            
-            board.Draw ();
-            ball.Draw();
-            player1.Draw();
-            player2.Draw();
-
-            while(playing){
-                player2.CleanDraw();
-                if(player2.WillEntityHitBarrier(
-                    board.GetBorderTop(),
-                    board.GetBorderBottom(),
-                    board.GetBorderLeft(),
-                    board.GetBorderRight())
-                ){
-                    player2.ChangeDirection();
-                }
-                player2.Move();
-                player2.Draw();
-                
-                Thread.Sleep(this.speed);
-            }
-
-            // this.Start();
+            this.Start ();
         }
 
-            
+        private void Start () {
+            bool playing = true;
+
+            GameBoard board = new GameBoard (
+                this.boardWidth, 
+                this.boardHeight, 
+                this.boardXOffset, 
+                this.boardYOffset
+            );
+            Ball ball = new Ball (
+                board.GetCenterHorizontal (),
+                board.GetCenterVertical (),
+                ConsoleColor.Green
+            );
+            Player player1 = new Player (
+                "John", 
+                board.GetBorderLeft () + 2, 
+                board.GetCenterVertical (),
+                board.GetBorderTop(),
+                board.GetBorderBottom(),
+                this.playerSize,
+                ConsoleColor.Blue
+            );
+            Bot player2 = new Bot (
+                "Bot", 
+                board.GetBorderRight () - 2, 
+                board.GetCenterVertical (),
+                board.GetBorderTop(),
+                board.GetBorderBottom(),
+                this.playerSize
+            );
+
+            board.Draw ();
+            ball.Draw ();
+            player1.Draw ();
+            player2.Draw ();
+
+            while (playing) {
+                player2.CleanDraw ();
+                if (player2.WillEntityHitBarrier (
+                        board.GetBorderTop (),
+                        board.GetBorderBottom (),
+                        board.GetBorderLeft (),
+                        board.GetBorderRight ())) {
+                    player2.ChangeDirection ();
+                }
+                player2.Move ();
+                player2.Draw ();
+
+                Thread.Sleep (this.speed);
+            }
+        }
+
         // }
 
         // private void KeypressListener()
         // {
 
         //     DateTime lastDrawnProjectile = DateTime.MinValue;
-            
+
         //     ConsoleKey key;
         //     do
         //     {
         //         /*
         //          * while (!Console.KeyAvailable) { }
         //          */
-                
 
         //         key = Console.ReadKey(true).Key;
 
