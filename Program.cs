7d4b9d52f3ec37db822ace5876bd919cb4183bd3
Benjamin Fransson <benjamin@cafcon.se>
2019-03-15 21:44:08 +0100

    Added menu

--- Program.cs
+++ Program.cs
@@ -4,7 +4,20 @@ namespace Pong {
     class Program {
         static void Main (string[] args) {
 
-            // Build a menu
+            Console.Clear();
+
+            Console.WriteLine("Welcome to Pong!");
+            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Press Q to exit the game at any time.");
+
+            if(Console.ReadKey().Key == ConsoleKey.Q){
+                System.Environment.Exit(0);
+            }
 
             Game game = new Game ();
 
